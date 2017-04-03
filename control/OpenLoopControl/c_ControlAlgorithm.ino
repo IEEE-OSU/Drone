b85414b656ba79e21f4e941cc3c0357e02365893
RogerKassouf <kassouf.12@osu.edu>
2017-04-03 13:07:14 -0400

    Update c_ControlAlgorithm.ino
    Greatly simplified the control algorithm to work well with writeMicroseconds() - customization is currently disabled.

--- control/OpenLoopControl/c_ControlAlgorithm.ino
+++ control/OpenLoopControl/c_ControlAlgorithm.ino
@@ -3,7 +3,7 @@
     Author: Aaron Pycraft, Roger Kassouf
     Contributors: Roger Kassouf, Iwan Martin, Chen Liang, Aaron Pycraft
 
-    Date last modified: 
+    Date last modified: 2017-03-04
 
     Objective: sketch contains functions that convert tx signals into motor
                control signals. 
@@ -20,18 +20,11 @@
 
 //--Constants used throughout algorithm
 // kState1Min, kState1Max; need not be declared b/c of forced compilation order
-const int kState3resolution = kState1Max - kState1Min;
-const int kState3Midpoint = kState1Max/2;
+const int kState3Midpoint = (kState1Max-kState1Min)/2; // 100
 
 //--Motor transformation constants
-const unsigned int kThrottleBound = (kState0Max - kState0Min)/4;
-const unsigned int kRotationsBound = (kThrottleBound/2)-1;
-const int kNI = 25;
-const int kNY = 5;
-const int kI = kRotationsBound/(kNI);
-const int kY = kRotationsBound/(kNY);
-const int Ndiff = (kServoMax-kServoMin)-kNI-kNY;
-const int TCut = (kThrottleBound*(kNI+kNY)/Ndiff) + 1;
+const int kThrottleBound = (kState0Max - kState0Min)/4; // 200
+const int kRotationsBound = (kThrottleBound/2)-1; // 100
 
 //--Variables used in controlTransfer
   int T, R, P, Y;   // local copies of the 4 tx signals
@@ -43,13 +36,6 @@ const int TCut = (kThrottleBound*(kNI+kNY)/Ndiff) + 1;
  *  OUTPUT: motorsOut = [M1 M2 M3 M4]
  */
 void controlTransfer(const unsigned int *txSignal, unsigned int *motorsOut) {
-  //--For debugging, simply make all motors behave the same way
-  motorsOut[0] = map(txSignal[THROTTLE], kState1Min, kState1Max, kServoMin, kServoMax);
-  motorsOut[1] = map(txSignal[THROTTLE], kState1Min, kState1Max, kServoMin, kServoMax);
-  motorsOut[2] = map(txSignal[THROTTLE], kState1Min, kState1Max, kServoMin, kServoMax);
-  motorsOut[3] = map(txSignal[THROTTLE], kState1Min, kState1Max, kServoMin, kServoMax);
-  /*
-  
   // abandon array in favor of more readable operations on the signals
   // the output will be contained in motorsOut anyways
   // probably a bad idea to read/write the same place VERY FAST
@@ -65,30 +51,22 @@ void controlTransfer(const unsigned int *txSignal, unsigned int *motorsOut) {
   
   // Transform to state 4 (skip 3); map values from 0 to state 3 resolution
   // to get lower bound of txSignal to be 0 to $STATE3_MAX_VALUE
-  T = T - kState1Min;
-  R = R - kState1Min - kState3Midpoint;
-  P = P - kState1Min - kState3Midpoint;
-  Y = Y - kState1Min - kState3Midpoint;
+  NT = T - kState1Min;
+  NP = constrain(P - kState1Min - kState3Midpoint,-NT/2,NT/2);
+  NR = constrain(R - kState1Min - kState3Midpoint,-NT/2,NT/2);
+  NY = constrain(Y - kState1Min - kState3Midpoint,-NT/2,NT/2);
+    // Ensures that no commands are executed which would give invalid Servo numbers.
   // state 4 achieved
-
-  NT = Ndiff*T/kThrottleBound;
-  NP = P/kI;
-  NR = R/kI;
-  NY = Y/kY;
-  
-  if (T < TCut) {
-    M1 = NT + kServoMin;
-    M2 = NT + kServoMin;
-    M3 = NT + kServoMin;
-    M4 = NT + kServoMin;
-  }
-  else {
-    M1 = NT + NP + NY + kServoMin;
-    M2 = NT + NR - NY + kServoMin;
-    M3 = NT - NP + NY + kServoMin;
-    M4 = NT - NR - NY + kServoMin;
-  }
-
+    
+  M1 = NT + NP + NY + kServoMin;
+  M2 = NT + NR - NY + kServoMin;
+  M3 = NT - NP + NY + kServoMin;
+  M4 = NT - NR - NY + kServoMin;
+  /* NOTE: This gives a potential value of any motor speed between 1200 and 1600.
+  in Servo microseconds. We have calibrated the ESCs for 2000, which indicates a
+  difference between 0% and 50% power at any given time. If one desired to use the
+  full power range, they would simply multiply {M1, M2, M3, M4} by 2. */
+    
   // Step 5: Re-map the speed values to each motor
   motorsOut[0] = M1;
   motorsOut[1] = M2;
@@ -97,4 +75,3 @@ void controlTransfer(const unsigned int *txSignal, unsigned int *motorsOut) {
   */
 
 }
-
